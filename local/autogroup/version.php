9c3f1211b684c2bf29abf262cc4d7a3c260bd46f
Mark Ward <mark.ward@gmail.com>
2014-12-24 13:20:24 +0000

    an initial commit including basic file structure and version file

---
+++ local/autogroup/version.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * autogroup local plugin
+ *
+ * This plugin automatically assigns users to a group within any course
+ * upon which they may be enrolled and which has auto-grouping
+ * configured.
+ *
+ * @package    local
+ * @subpackage autogroup
+ * @author     Mark Ward (me@moodlemark.com)
+ * @date       December 2014
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2014122400;
+$plugin->requires = 2013111800.00;      // Requires this Moodle version (2.7).
+$plugin->release = '0.1';               // Plugin release.
+$plugin->component = 'local_autogroup'; // Full name of the plugin (used for diagnostics).
+$plugin->maturity = MATURITY_ALPHA;     // Only just getting started.
