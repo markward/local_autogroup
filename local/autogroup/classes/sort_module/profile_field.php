f67a644b15dbb302cf640ac3b09b7a1633f1d7a7
Mark Ward <me@moodlemark.com>
2014-12-28 13:11:56 +0000

    initial commit for profile_field sort module

---
+++ local/autogroup/classes/sort_module/profile_field.php
@@ -0,0 +1,60 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * autogroup local plugin
+ *
+ * A course object relates to a Moodle course and acts as a container
+ * for multiple groups. Initialising a course object will automatically
+ * load each autogroup group for that course into memory.
+ *
+ * @package    local
+ * @subpackage autogroup
+ * @author     Mark Ward (me@moodlemark.com)
+ * @date       December 2014
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_autogroup\sort_module;
+
+use local_autogroup\sort_module;
+use local_autogroup\exception;
+
+/**
+ * Class course
+ * @package local_autogroup\domain
+ */
+class profile_field extends sort_module
+{
+    /**
+     * @param \stdclass $user
+     * @param int $courseid
+     * @param string $config
+     */
+    public function __construct(\stdclass $user, $courseid, $config)
+    {
+
+    }
+
+    /**
+     * @return array $result
+     */
+    public function eligable_groups()
+    {
+
+    }
+
+}
