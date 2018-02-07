1e86a5eda33da9cbf7ff5097254589902dc9f875
emmarichardson <emmar@ecboces.org>
2018-02-07 12:59:43 -0700

    Update version.php

--- local/autogroup/version.php
+++ local/autogroup/version.php
@@ -30,7 +30,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 20180207;
+$plugin->version = 2018020700;
 $plugin->requires = 2013111800.00;       // Requires this Moodle version (2.7).
 $plugin->release = '2.3';                // Plugin release.
 $plugin->component = 'local_autogroup';  // Full name of the plugin (used for diagnostics).
