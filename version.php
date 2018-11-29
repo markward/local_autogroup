be834907db4a822f6974ca550f43284c864a815d
Nathaniel Sabanski <sabanski.n@gmail.com>
2018-11-29 15:11:02 -0800

    Revert version.php change.

--- version.php
+++ version.php
@@ -30,8 +30,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018102400;
+$plugin->version = 2018070300;
 $plugin->requires = 2013111800.00;       // Requires this Moodle version (2.7).
-$plugin->release = '2.4.1';                // Plugin release.
+$plugin->release = '2.4';                // Plugin release.
 $plugin->component = 'local_autogroup';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
